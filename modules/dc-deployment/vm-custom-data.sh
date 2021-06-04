02ade6f1dc26e9d374de881744f933d417892f0c
Carsten Duch <cduch@users.noreply.github.com>
2021-06-04 09:36:31 +0200

    Delete vm-custom-data.sh
    replaced with cloud-init.yml

--- modules/dc-deployment/vm-custom-data.sh
+++
@@ -1,6 +0,0 @@
-#! /bin/bash
-sudo apt-get update
-sudo apt-get install -y apache2 php libapache2-mod-php
-sudo systemctl start apache2
-sudo systemctl enable apache2
-echo "<?php echo \"<h1>You are visiting webserver: \".\$_SERVER['SERVER_NAME'].\"</h1>\";" | sudo tee /var/www/html/index.php
