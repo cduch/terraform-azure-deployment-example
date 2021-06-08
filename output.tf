633f35b65f161e68b0aa59f8c1db69d36853ca8e
Carsten Duch <carsten@hashicorp.com>
2021-06-08 18:11:41 +0200

    changed output

--- output.tf
+++ output.tf
@@ -4,6 +4,6 @@
 
 output "ip_addresses" {
     value = toset([
-        for name in module.vm-example-module.name : module.vm-example-module[name].ip_address
+        for key in module.vm-example-module.key : module.vm-example-module[key].ip_address
     ])
 }
